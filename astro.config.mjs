6ead2b041159b3081854ff9c7d8014bc5c220116
andreisudarikov19 <andreisudarikov19@icloud.com>
2025-06-08 18:45:16 +0200

    remove markdoc import from astro.comfig.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import markdoc from '@astrojs/markdoc';	
 
 // https://astro.build/config
 export default defineConfig({
