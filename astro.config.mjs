9c5699afc6648dd9106f0d6e27ac0af1f9c10399
andreisudarikov19 <andreisudarikov19@icloud.com>
2025-06-08 17:55:34 +0200

    re-introduce markdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import markdoc from '@astrojs/markdoc';	
 
 // https://astro.build/config
 export default defineConfig({
